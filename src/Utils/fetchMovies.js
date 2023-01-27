44678a630d710842f4e5a0f4384a4a282a00d516
jss118 <joesmith_guitar@hotmail.co.uk>
2023-01-27 16:59:23 +0000

    add filter which then gives us a single movie page

--- src/Utils/fetchMovies.js
+++ src/Utils/fetchMovies.js
@@ -1,6 +1,5 @@
 export const fetchMovies = async () => {
   const res = await fetch("http://localhost:3000/movies");
   const movies = await res.json();
-  console.log(movies);
   return movies;
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="header_h1">Movie Flicker</h1>
+      <Link
+        style={{ textDecoration: "none", marginLeft: 100, marginRight: 100 }}
+        to="/"
+      >
+        <h1 className="header_h1">Movie Flicker</h1>
+      </Link>
       <p className="header_p">Search. Discover. Immerse.</p>
       <SearchBar />
     </header>
